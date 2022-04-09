68d2e67de742418dc96ac928d870924a34881233
d4rklynk <74207682+d4rklynk@users.noreply.github.com>
2022-04-09 13:06:58 +0000

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -6,16 +6,16 @@
 
 /* override recipe: enable session restore ***/
 user_pref("browser.startup.page", 3); // 0102
-  // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
-  // user_pref("places.history.enabled", true); // 0862 required if you had it set as false
+   // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
+   // user_pref("places.history.enabled", true); // 0862 required if you had it set as false
 user_pref("browser.sessionstore.privacy_level", 0); // 1003 [to restore cookies/formdata if not sanitized]
-  // user_pref("network.cookie.lifetimePolicy", 0); // 2801 [DON'T: add cookie + site data exceptions instead]
+   // user_pref("network.cookie.lifetimePolicy", 0); // 2801 [DON'T: add cookie + site data exceptions instead]
 user_pref("privacy.clearOnShutdown.history", false); // 2811
-  // user_pref("privacy.clearOnShutdown.cookies", false); // 2811 optional: default false arkenfox v94
-  // user_pref("privacy.clearOnShutdown.formdata", false); // 2811 optional
+   // user_pref("privacy.clearOnShutdown.cookies", false); // 2811 optional: default false arkenfox v94
+   // user_pref("privacy.clearOnShutdown.formdata", false); // 2811 optional
 user_pref("privacy.cpd.history", false); // 2812 to match when you use Ctrl-Shift-Del
-  // user_pref("privacy.cpd.cookies", false); // 2812 optional: default false arkenfox v94
-  // user_pref("privacy.cpd.formdata", false); // 2812 optional
+   // user_pref("privacy.cpd.cookies", false); // 2812 optional: default false arkenfox v94
+   // user_pref("privacy.cpd.formdata", false); // 2812 optional
 
   /* 1601: control when to send a cross-origin referer
  * 0=always (default), 1=only if base domains match, 2=only if hosts match
